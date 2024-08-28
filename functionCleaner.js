52ababfe85f095bf4197f1ea7353a7daeddc666f
Lucas Silva <L.Silva.E@hotmail.com>
2024-08-27 22:41:28 -0400

    🚚 Moved functions to copy data

--- functionCleaner.js
+++ functionCleaner.js
@@ -308,39 +308,6 @@ function addAndCleanNewRows () {
   showMessage('🧼 Limpieza finalizada', messageBody);
 }
 
-
-function createOrUpdateBackup (dataConfigSheet) {
-  const sheetResponses = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(dataConfigSheet.SHEET_RESPONSES);
-  let sheetBackup = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(dataConfigSheet.SHEET_BACKUP);
-  let messageHeader = '🔃 Actualizando el respaldo';
-  let messageBody = 'Copiando los datos de la "Hoja de Respuestas" a la "Hoja de Respaldo"';
-
-  //~ En caso de no existir, se crea la pestaña de respaldo ~//
-  if (sheetBackup === null) {
-    messageHeader = '⚠️ Creando respaldo';
-    messageBody = 'Creando el respaldo con los datos de la "Hoja de Respuestas"';
-    sheetBackup = SpreadsheetApp.getActiveSpreadsheet().insertSheet();
-    sheetBackup.setName(dataConfigSheet.SHEET_BACKUP);
-  }
-
-  showToast(messageHeader, messageBody);
-
-  let sheetSource = sheetResponses.getRange(1, 1, sheetResponses.getLastRow(), sheetResponses.getLastColumn());
-
-  let rowRange = sheetBackup.getLastRow() || sheetResponses.getLastRow();
-  let columnRange = sheetBackup.getLastColumn() || sheetResponses.getLastColumn();
-  let sheetDestination = sheetBackup.getRange(1, 1, rowRange, columnRange);
-  sheetDestination.clearContent();
-
-  sheetDestination = sheetBackup.getRange(1, 1, sheetResponses.getLastRow(), sheetResponses.getLastColumn());
-
-  sheetSource.copyTo(sheetDestination);
-
-  sheetDestination = sheetBackup.getRange(1, 1, sheetBackup.getMaxRows(), sheetBackup.getMaxColumns());
-  sheetDestination.setNumberFormat('@');
-}
-
-
 function getDataConfigSheet () {
   let sheetConfig = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(getConfigSheet().SHEET_CONFIG.value);
   let dataConfigSheet = {};
